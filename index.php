080d79b808b330a8654c9a6181239b23785dc909
gwend <gwend@jvocab.com>
2019-01-14 22:58:56 +0200

    301 Moved Permanently

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 if(substr($_SERVER['HTTP_HOST'],0,4)==='www.') {
+    header("HTTP/1.1 301 Moved Permanently");
     header('Location: http://'.substr($_SERVER['HTTP_HOST'],4).$_SERVER['REQUEST_URI']);
     exit;
 }
